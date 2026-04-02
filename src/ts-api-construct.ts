9b9e64d9e8c07cdc85b8409d573a16cbb98017fc
Nikit Zykov <nikit@zykov.com>
2026-04-02 20:30:31 +0200

    Allowed the S3 bucket reader to list contents

--- src/ts-api-construct.ts
+++ src/ts-api-construct.ts
@@ -1213,12 +1213,20 @@ export class TSApiConstruct<T extends ApiDefinition> extends Construct {
             bucket.addToResourcePolicy(
                 new PolicyStatement({
                     effect: Effect.ALLOW,
-                    actions: bucketProps?.needsListing === false ? ['s3:PutObject', 's3:DeleteObject', 's3:GetObject'] :
-                        ['s3:PutObject', 's3:DeleteObject', 's3:GetObject', 's3:ListBucket'],
+                    actions: ['s3:PutObject', 's3:DeleteObject', 's3:GetObject'],
                     resources: [bucket.arnForObjects("*")],
                     principals: [bucketUser.grantPrincipal],
                 })
             )
+
+            if (bucketProps?.needsListing !== false) {
+                bucket.addToResourcePolicy(new PolicyStatement({
+                    effect: Effect.ALLOW,
+                    actions: ['s3:ListBucket'],
+                    resources: [bucket.bucketArn],
+                    principals: [bucketUser.grantPrincipal],
+                }))
+            }
 
             const accessKey = new CfnAccessKey(scope, `S3Bucket-${props.apiName}-${props.deployFor}-${bucketName}-bucket-user-access-key`, {
                 userName: bucketUser.userName,
